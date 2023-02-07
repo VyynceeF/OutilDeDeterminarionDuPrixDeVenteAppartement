03078a7afa5203207915b410d9673e5a263d5e99
vincent <vincent.faure@iut-rodez.fr>
2023-02-07 10:31:24 +0100

    Add commentaires

--- openfile.py
+++ openfile.py
@@ -1,11 +1,13 @@
+#Ouverture d'un fichier et renvoie des valeurs dans un tableau
+#Le paramètre name est le chemin du fichier
 def openFile(name):
     tab = []
-    file = open(name, "r")
-    lines = file.readlines()
-    for line in lines:
-        surface = float(line.split()[0])
-        prx = float(line.split()[1])
-        tab.append([surface, prx])
+    file = open(name, "r") #Ouvre le fichier
+    lines = file.readlines() #Récupère les lignes du fichiers
+    for line in lines: #On boucle dans les lignes
+        surface = float(line.split()[0]) #Recupere la surface
+        prx = float(line.split()[1]) #Recupere le prix
+        tab.append([surface, prx]) #Ajoute au tableau le sous-tableau [surface, prix]
     return tab
 
 
