273a65fe8d5b54278880a574f87b0fa394c2ff43
vincent <vincent.faure@iut-rodez.fr>
2023-02-07 10:05:15 +0100

    Add function moyenneCarree

--- Outils.py
+++ Outils.py
@@ -1,8 +1,22 @@
 import openfile
 def analytique() :
-    tab = openfile("donnees.txt")
 
-def moyenne() :
+    tabX = []
+    tabY = []
+    tab = openfile.openfile("donnees.txt")
 
+    for val in tab:
+        tabX.append(val[0])
+        tabY.append(val[1])
+
+    minimun = (moyenneXY(tab) - moyenneX(tabX) * moyenneX(tabY)) / moyenneCarre(tabX) - moyenneX(tabX)**2
+
+    return minimun
+
+def moyenneCarree(tab):
+    somme = 0
+    for val in tab:
+        somme += val**2
+    return somme/ len(tab)
 
 analytique()
