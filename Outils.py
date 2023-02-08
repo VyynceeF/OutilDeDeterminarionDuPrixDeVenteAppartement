4ff204530d930929e85acc598e9114b203d6c88b
Antoine Gouzy <antoinegp.gouzy@gmail.com>
2023-02-08 10:14:09 +0100

    Ajout derivee de x

--- Outils.py
+++ Outils.py
@@ -1,10 +1,12 @@
 import math
-
 import openfile
 
-#Calcul du point critique qui correspond à un minimum
-#Le point critique est le point où s'annule le gradient
 def analytique() :
+    """
+    Determine le minimum d'une fonction de maniere analytique
+    en applicant la formule "cov(x,y) / V(x)"
+    :return: la pente et l'ordonnée à l'origin
+    """
 
     tabX = [] #Déclaration tableau des x
     tabY = [] #Déclaration tableau des y
@@ -22,72 +24,80 @@ def analytique() :
 
     return (a,b)
 
-#Calcul la moyenne des valeurs aux carrée du tableau tab
-def moyenneCarree(tab):
+def moyenneCarree(tableau):
+    """
+    Calcul la moyenne des valeurs au carrée d'un tableau
+    :param tableau: [x1,...,xn]
+    :return: la moyenne des variables d'un tableau
+    """
     somme = 0
-    for val in tab:
+    for val in tableau:
         somme += val ** 2
-    return somme / len(tab)
+    return somme / len(tableau)
 
-#Calcul la moyenne des valeurs du tableau
 def moyenneX(tableau):
+    """
+    Calcul la moyenne des valeurs d'un tableau
+    :param tableau: tableau contenant les données en x, tableau [x1,...,xn]
+    :return: la moyenne d'un tableau contenant qu'une variable
+    """
     somme = 0
     for val in tableau:
         somme += val
     return somme / len(tableau)
 
-#Calcul la moyenne des x*y du tableau (tableau = [[x1,y1],[x2,y2]])
 def moyenneXY(tableau):
+    """
+    Calcul la moyenne des x * y du tableau
+    :param tableau: tableau contenant toutes les donnees, tableau = [[x1,y1],...,[xn,yn]]
+    :return: la moyenne des xy
+    """
     nouveauTableau = []
     for val in tableau:
         nouveauTableau.append(val[0]*val[1])
     return moyenneX(nouveauTableau)
 
 def gradient(a, b) :
-    tabX = []  # Déclaration tableau des x
-    tabY = []  # Déclaration tableau des y
+    """
+    Calcul le gradient de a et b
+    :param a: pente de la droite
+    :param b: ordonnée à l'origine
+    :return:  les derives partielles de a et b
+    """
     tab = openfile.openFile("donnees.txt")  #Récupération des valeurs du fichier
-
-    # On insere les x et les y dans tabX et tabY
-    for val in tab:
-        tabX.append(val[0])
-        tabY.append(val[1])
 
     sommeXi = 0
     sommeX2 = 0
     sommeYi = 0
-
-    for i in tabX:
-        sommeXi += i
-        sommeX2 += i ** 2
-
     sommeXY = 0
+
     for i in tab:
-        sommeXY += i[0]*i[1]
+        sommeXi += i[0]
+        sommeX2 += i[0]**2
+        sommeYi += i[1]
+        sommeXY += i[0] * i[1]
 
     deriveX = 2 * (a * sommeX2 + b * sommeXi - sommeXY)
-    deriveY = 2 * (a * sommeXi + len(tabX) * b - sommeYi)
+    deriveY = 2 * (a * sommeXi + len(tab) * b - sommeYi)
 
     return (deriveX, deriveY)
 
 def gradientDescent():
+    """
+    Determine le minimum d'une fonction  en appliquant la descente de gradient
+    :return: la pente et l'ordonnée à l'origin au point minimum
+    """
     a = 1
     b = 1
-    alpha = 0.001
+    alpha = 0.00001
     derives = gradient(a, b)
-    print(derives)
     normeGrad = math.sqrt(derives[0]**2+derives[1]**2)
     while(abs(normeGrad)>1e-6) :
         derives = gradient(a,b)
         normeGrad = math.sqrt(derives[0]**2+derives[1]**2)
         a = a - alpha * derives[0]
         b = b - alpha * derives[1]
-        print(a,b)
     return (a,b)
 
 print(analytique())
-#print(gradient(2.6370100601552733,3.527390358018039))
 print(gradientDescent())
-
-#Si a negatif alors a gauche de la courbe -> augmenter b
-#Si a positif alors a droite de la courbe -> baisser b
