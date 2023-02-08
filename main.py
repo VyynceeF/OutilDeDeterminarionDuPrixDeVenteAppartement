d8343a2978a0f28f2e278d8134b24b2c048ba2a8
Antoine Gouzy <antoinegp.gouzy@gmail.com>
2023-02-08 10:27:23 +0100

    Modification du main

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import Outils
 
-print("1 - Méthode analytique\n2 - Déscente de gradient")
+option = input("1 - Méthode analytique\n2 - Descente de gradient\n")
+
+while (option != 10) :
+    match option:
+        case "1":
+            print(Outils.analytique())
+
+        case "2":
+            print(Outils.gradientDescent())
+
+        case _:
+            print("Option choisi incorrecte")
+
+print("Au revoir")
