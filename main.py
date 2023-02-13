91f32d01769ae4e01fe0a8befa8c8c562865b2a6
vincent <vincent.faure@iut-rodez.fr>
2023-02-13 12:30:09 +0100

    commit update traitement et main

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import Outils
+import Outils, os, traitement
 
-option = input("1 - Méthode analytique\n2 - Descente de gradient\n")
+option = input("1 - Méthode analytique\n2 - Descente de gradient\n3 - Créer et initialiser votre fichier de données")
 
-while (option != 10) :
+while (option != "10") :
     match option:
         case "1":
             print(Outils.analytique())
-
         case "2":
             print(Outils.gradientDescent())
-
+        case "3":
+            name = str(input("Entrer le nom du fichier à créer : ")) + ".txt"
+            traitement.creation(name)
         case _:
             print("Option choisi incorrecte")
 
