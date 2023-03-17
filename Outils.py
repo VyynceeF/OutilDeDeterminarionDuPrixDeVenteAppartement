72ae59f39e15970bd235d35f13532011ed6acd73
Antoine Gouzy <antoinegp.gouzy@gmail.com>
2023-03-17 08:09:09 +0100

    Essai optimisation alpha

--- Outils.py
+++ Outils.py
@@ -1,28 +1,30 @@
 import math
 import openfile
 
-def analytique() :
+
+def analytique():
     """
     Determine le minimum d'une fonction de maniere analytique
     en applicant la formule "cov(x,y) / V(x)"
     :return: la pente et l'ordonnée à l'origin
     """
 
-    tabX = [] #Déclaration tableau des x
-    tabY = [] #Déclaration tableau des y
-    tab = openfile.openFile("donnees.txt") #Récupération des valeurs du fichier
+    tabX = []  # Déclaration tableau des x
+    tabY = []  # Déclaration tableau des y
+    tab = openfile.openFile("donnees.txt")  # Récupération des valeurs du fichier
 
-    #On insere les x et les y dans tabX et tabY
+    # On insere les x et les y dans tabX et tabY
     for val in tab:
         tabX.append(val[0])
         tabY.append(val[1])
 
-    #Calcul de a
-    a = (moyenneXY(tab) - moyenneX(tabX) * moyenneX(tabY)) / (moyenneCarree(tabX) - (moyenneX(tabX)**2))
-    #Calcul de b
+    # Calcul de a
+    a = (moyenneXY(tab) - moyenneX(tabX) * moyenneX(tabY)) / (moyenneCarree(tabX) - (moyenneX(tabX) ** 2))
+    # Calcul de b
     b = moyenneX(tabY) - a * moyenneX(tabX)
 
-    return (a,b)
+    return (a, b)
+
 
 def moyenneCarree(tableau):
     """
@@ -35,6 +37,7 @@ def moyenneCarree(tableau):
         somme += val ** 2
     return somme / len(tableau)
 
+
 def moyenneX(tableau):
     """
     Calcul la moyenne des valeurs d'un tableau
@@ -46,6 +49,7 @@ def moyenneX(tableau):
         somme += val
     return somme / len(tableau)
 
+
 def moyenneXY(tableau):
     """
     Calcul la moyenne des x * y du tableau
@@ -54,17 +58,18 @@ def moyenneXY(tableau):
     """
     nouveauTableau = []
     for val in tableau:
-        nouveauTableau.append(val[0]*val[1])
+        nouveauTableau.append(val[0] * val[1])
     return moyenneX(nouveauTableau)
 
-def gradient(a, b) :
+
+def gradient(a, b):
     """
     Calcul le gradient de a et b
     :param a: pente de la droite
     :param b: ordonnée à l'origine
     :return:  les derives partielles de a et b
     """
-    tab = openfile.openFile("donnees.txt")  #Récupération des valeurs du fichier
+    tab = openfile.openFile("donnees.txt")  # Récupération des valeurs du fichier
 
     sommeXi = 0
     sommeX2 = 0
@@ -73,7 +78,7 @@ def gradient(a, b) :
 
     for i in tab:
         sommeXi += i[0]
-        sommeX2 += i[0]**2
+        sommeX2 += i[0] ** 2
         sommeYi += i[1]
         sommeXY += i[0] * i[1]
 
@@ -82,19 +87,42 @@ def gradient(a, b) :
 
     return (deriveX, deriveY)
 
+
 def gradientDescent():
     """
     Determine le minimum d'une fonction  en appliquant la descente de gradient
-    :return: la pente et l'ordonnée à l'origin au point minimum
+    :return: la pente et l'ordonnée à l'origine au point minimum
     """
-    a = 1
-    b = 1
-    alpha = 0.00001
+    a = 32
+    b = 69.5
+    alpha = 0.00002
     derives = gradient(a, b)
-    normeGrad = math.sqrt(derives[0]**2+derives[1]**2)
-    while(abs(normeGrad)>1e-6) :
-        derives = gradient(a,b)
-        normeGrad = math.sqrt(derives[0]**2+derives[1]**2)
+    derives_temp = derives
+    normeGrad = math.sqrt(derives[0] ** 2 + derives[1] ** 2)
+
+    while normeGrad > 1e-5:
         a = a - alpha * derives[0]
         b = b - alpha * derives[1]
-    return (a,b)
+
+        derives_temp,derives = derives,gradient(a, b)
+
+        if 0 < derives[0] and derives[0] > derives_temp[0] \
+                or 0 < derives[1] and derives[1] > derives_temp[1] \
+                or 0 > derives[0] and derives[0] < derives_temp[0] \
+                or 0 > derives[1] and derives[1] < derives_temp[1]:
+            alpha = alpha / 2
+
+        else:
+            print(alpha, normeGrad, a, b, derives)
+
+        normeGrad = math.sqrt(derives[0] ** 2 + derives[1] ** 2)
+
+    return (a, b)
+
+print(analytique())
+print(gradientDescent())
+
+"""
+alpha -> si pente iteration precedente negative est pente de cette iteration positive reduire alpha 
+alpha -> petit si ecart entre les deux normes est petit
+"""
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import Outils
+from tkinter import *
+from tkinter import filedialog
 
-option = input("1 - Méthode analytique\n2 - Descente de gradient\n")
+def fileOpen():
+    filename = filedialog.askopenfilename(initialdir="/",
+                                          title="Select a File",
+                                          filetypes=(("Text files",
+                                                      "*.txt*"),
+                                                     ("all files",
+                                                      "*.*")))
+    label.configure(text="Fichier actuel: " + filename)
 
-while (option != 10) :
-    match option:
-        case "1":
-            print(Outils.analytique())
 
-        case "2":
-            print(Outils.gradientDescent())
+window = Tk()
+window.title("Outil d'aide à la determination")
+window.geometry('500x500')
+window.resizable(width = 0, height = 0)
+window["bg"] = "#e1e6ec"
 
-        case _:
-            print("Option choisi incorrecte")
+label = Label(window, text="Fichier actuel : ")
+label.pack()
 
-print("Au revoir")
+saisie = Entry(window, width=10)
+saisie.pack()
+
+bouton = Button( window , text = "Choisir un fichier", command=fileOpen)
+bouton.pack()
+
+window.mainloop()
