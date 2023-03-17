b001b8115793ae5abcaf6de9e4ff9a8d8d68ba00
Antoine Gouzy <115453602+AntoineGouzy@users.noreply.github.com>
2023-03-17 10:15:51 +0100

    Add files via upload

--- Outils.py
+++ Outils.py
@@ -1,27 +1,38 @@
 import math
 import openfile
 
+from statistics import median
+from math import isnan
+from itertools import filterfalse
 
-def analytique():
-    """
-    Determine le minimum d'une fonction de maniere analytique
-    en applicant la formule "cov(x,y) / V(x)"
-    :return: la pente et l'ordonnée à l'origin
-    """
+
+def formatageDonnees(donnees):
 
     tabX = []  # Déclaration tableau des x
     tabY = []  # Déclaration tableau des y
-    tab = openfile.openFile("donnees.txt")  # Récupération des valeurs du fichier
+    tab = openfile.openFile(donnees)  # Récupération des valeurs du fichier
 
     # On insere les x et les y dans tabX et tabY
     for val in tab:
         tabX.append(val[0])
         tabY.append(val[1])
 
+    return(tabX, tabY)
+
+def analytique(donnees):
+    """
+    Determine le minimum d'une fonction de maniere analytique
+    en applicant la formule "cov(x,y) / V(x)"
+    :return: la pente et l'ordonnée à l'origin
+    """
+    tab = formatageDonnees(donnees)
+    tabA = tab[0]  # Déclaration tableau des x
+    tabB = tab[1]  # Déclaration tableau des y
+    
     # Calcul de a
-    a = (moyenneXY(tab) - moyenneX(tabX) * moyenneX(tabY)) / (moyenneCarree(tabX) - (moyenneX(tabX) ** 2))
+    a = (moyenneXY(tabA, tabB) - moyenneX(tabA) * moyenneX(tabB)) / (moyenneCarree(tabA) - (moyenneX(tabA) ** 2))
     # Calcul de b
-    b = moyenneX(tabY) - a * moyenneX(tabX)
+    b = moyenneX(tabB) - a * moyenneX(tabA)
 
     return (a, b)
 
@@ -50,26 +61,31 @@ def moyenneX(tableau):
     return somme / len(tableau)
 
 
-def moyenneXY(tableau):
+def moyenneXY(tableauA, tableauB):
     """
     Calcul la moyenne des x * y du tableau
     :param tableau: tableau contenant toutes les donnees, tableau = [[x1,y1],...,[xn,yn]]
     :return: la moyenne des xy
     """
     nouveauTableau = []
-    for val in tableau:
-        nouveauTableau.append(val[0] * val[1])
+    for i in range (len(tableauA)):
+        nouveauTableau.append(tableauA[i] * tableauB[i])
     return moyenneX(nouveauTableau)
 
+def mediane(tableau):
 
-def gradient(a, b):
+    tableauPropre = list(filterfalse(isnan, tableau))  # Strip NaN values
+    sorted(tableauPropre)  # Sorting now works as expected
+    return median(tableauPropre)
+
+def gradient(a, b, donnees):
     """
     Calcul le gradient de a et b
     :param a: pente de la droite
     :param b: ordonnée à l'origine
     :return:  les derives partielles de a et b
     """
-    tab = openfile.openFile("donnees.txt")  # Récupération des valeurs du fichier
+    tab = openfile.openFile(donnees)  # Récupération des valeurs du fichier
 
     sommeXi = 0
     sommeX2 = 0
@@ -88,39 +104,38 @@ def gradient(a, b):
     return (deriveX, deriveY)
 
 
-def gradientDescent():
+def gradientDescent(donnees):
     """
     Determine le minimum d'une fonction  en appliquant la descente de gradient
     :return: la pente et l'ordonnée à l'origine au point minimum
     """
-    a = 32
-    b = 69.5
-    alpha = 0.00002
-    derives = gradient(a, b)
-    derives_temp = derives
+
+    tableau = formatageDonnees(donnees)
+    a = mediane(tableau[0])
+    b = mediane(tableau[1])
+    print (a, b)
+    ite = 0
+    alpha = 0.001
+    derives = gradient(a, b, donnees)
+
     normeGrad = math.sqrt(derives[0] ** 2 + derives[1] ** 2)
 
     while normeGrad > 1e-5:
-        a = a - alpha * derives[0]
+        ite += 1
+        a = a - alpha * derives[0]        
         b = b - alpha * derives[1]
-
-        derives_temp,derives = derives,gradient(a, b)
-
-        if 0 < derives[0] and derives[0] > derives_temp[0] \
-                or 0 < derives[1] and derives[1] > derives_temp[1] \
-                or 0 > derives[0] and derives[0] < derives_temp[0] \
-                or 0 > derives[1] and derives[1] < derives_temp[1]:
-            alpha = alpha / 2
-
-        else :
-            print(alpha, normeGrad, a, b, derives)
-
+        derives = gradient(a, b, donnees)
         normeGrad = math.sqrt(derives[0] ** 2 + derives[1] ** 2)
+        print(a, b)
 
-    return (a, b)
+        if  ancienA < a and ancienB < b :
+            alpha /= 100
+        
+    return (a, b, ite)
 
-print(analytique())
-print(gradientDescent())
+donnees = "donnees.txt"
+print(analytique(donnees))
+print(gradientDescent(donnees))
 
 """
 alpha -> si pente iteration precedente negative est pente de cette iteration positive reduire alpha 
