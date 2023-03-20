e353543b8971221adf17836509293cffd831dfe2
vincent <vincent.faure@iut-rodez.fr>
2023-03-20 10:41:54 +0100

    commit merge

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def fileOpen():
     label.configure(text="Fichier actuel :\n" + filename)
 
 def creer():
-    os.system("py traitement.py")
+    os.system("py traitement.py") #Test
 
 def calculAnalytique():
     global filename
